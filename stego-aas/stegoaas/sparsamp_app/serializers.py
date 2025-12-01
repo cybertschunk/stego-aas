99b807630fbc22cacf7043a63c157956997c21ff
David Leeuwestein <david@leeuwestein.net>
2025-12-01 13:07:40 +0100

    serializers are no longer cleaning up whitespace.

--- stego-aas/stegoaas/sparsamp_app/serializers.py
+++ stego-aas/stegoaas/sparsamp_app/serializers.py
@@ -4,22 +4,20 @@ from rest_framework import serializers
 
 
 class EncodeRequestSerializer(serializers.Serializer):
-    plaintext = serializers.CharField()
-    context = serializers.CharField()
+    plaintext = serializers.CharField(trim_whitespace=False)
+    context = serializers.CharField(trim_whitespace=False)
     random_seed = serializers.IntegerField()
 
 
 class DecodeRequestSerializer(serializers.Serializer):
     messages = serializers.ListField(
-        child=serializers.CharField()
+        child=serializers.CharField(trim_whitespace=False)
     )
-    context = serializers.CharField()
+    context = serializers.CharField(trim_whitespace=False)
     random_seed = serializers.IntegerField()
 
 class MessagesSerializer(serializers.Serializer):
     messages = serializers.ListField(
-        child=serializers.CharField()
+        child=serializers.CharField(trim_whitespace=False)
     )
 
-class MessageSerializer(serializers.Serializer):
-    message = serializers.CharField()
--- stego-aas/stegoaas/sparsamp_app/views.py
+++ stego-aas/stegoaas/sparsamp_app/views.py
@@ -4,10 +4,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import EncodeRequestSerializer, DecodeRequestSerializer, MessagesSerializer, MessageSerializer
+from .serializers import EncodeRequestSerializer, DecodeRequestSerializer, MessagesSerializer
 from .encoding import full_encode
 from .decoding import full_decode
-from .sparsamp_utils import string_to_utf8_binary
 
 
 class SparsampEncodeView(APIView):
